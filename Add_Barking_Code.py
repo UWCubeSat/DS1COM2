b0f5ba867a8d7240e4fd2375783756ba4a7d6d82
Garrett Finucane <garrettdreyfus@gmail.com>
2018-11-04 12:56:37 -0800

    changed some stuff up

---
+++ Add_Barking_Code.py
@@ -0,0 +1,33 @@
+import numpy as np
+from gnuradio import gr
+
+
+class blk(gr.basic_block):  # other base classes are basic_block, decim_block, interp_block
+    """Embedded Python Block example - a simple multiply const"""
+
+    def __init__(self):  # only default arguments here
+        """arguments to this function show up as parameters in GRC"""
+        gr.basic_block.__init__(self, name="Add Barking Code",
+            in_sig=[np.uint8],
+            out_sig=[np.uint8])
+        # if an attribute with the same name as a parameter is found,
+        # a callback is registered (properties work, too).
+
+	self.barkersequence = [0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0]*2
+	self.syncLength = len(self.barkersequence)
+	self.PACKET_SIZE = 512
+
+
+    def general_work(self, input_items, output_items):
+        """example: multiply with constant"""
+	#TAKES in unpacked bytes and adds barker codes
+	if len(input_items[0]) > self.PACKET_SIZE and len(output_items[0]) > self.PACKET_SIZE + self.syncLength:
+		for i in range(len(self.barkersequence)):
+			output_items[0][i] = self.barkersequence[i]
+		for i in range(self.PACKET_SIZE):
+			output_items[0][self.syncLength+i] = input_items[0][i]
+		self.consume_each(self.PACKET_SIZE);
+        	return self.PACKET_SIZE + self.syncLength
+	return 0
+
+
---
+++ epy_block_0.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+from gnuradio import gr
+from gnuradio import blocks
+from gnuradio import digital
+import numpy as np
+
+np.set_printoptions(threshold=np.nan)
+
+_tracep = False
+def tprint(msg):
+    if _tracep:
+        print msg
+
+
+class barker_sync(gr.basic_block):
+    def __init__(self):
+        gr.basic_block.__init__(self, name="barker_sync",
+            in_sig=[np.uint8],
+            out_sig=[np.uint8])
+
+        ##################################################
+        # Put Variables Here
+        ##################################################
+        self.syncSize = 32
+        self.packetSize = 512
+        self.tolerance = 0
+        self.syncAndPacketSize = self.syncSize + self.packetSize
+        self.barkersequence = np.array([-1, -1, -1, -1, -1, 1, 1, -1, -1, 1, -1, 1, -1, -1, -1, -1]*2)
+
+    def general_work(self, input_items, output_items):
+
+        # Initial housekeeping
+        in0 = input_items[0]
+        out = output_items[0]
+
+#        print("Input/Output Items:  {}/{}".format(len(in0), len(out)))
+
+        # First, make sure the buffer has enough bits to be able to process
+        if len(in0) < self.syncAndPacketSize:
+            self.consume_each(int(0))
+            return 0
+        # scan for sync pattern
+        correlationOutput = np.correlate(in0.astype(np.int32) * 2 - 1, self.barkersequence)
+        maxCorrIndex = np.argmax(correlationOutput)
+	maxCorrValue = correlationOutput[maxCorrIndex]
+
+        # if a lousy match, reset the buffers to try again
+        if maxCorrValue < (self.syncSize - self.tolerance):
+            tprint("Barker correlation insufficient, resetting buffer.")
+            self.consume_each(int(len(in0)))
+            return 0
+
+        # Good match - do we have room?
+        # TODO:  maybe have another interim state that allows us to avoid scanning for the Barker again
+        if len(in0) < (maxCorrIndex + self.syncAndPacketSize):
+            tprint("Barker found, but need more data - filling buffer.")
+            self.consume_each(int(maxCorrIndex - 1))
+            return 0
+        index =0
+        for i in in0[maxCorrIndex + self.syncSize : maxCorrIndex + self.syncAndPacketSize-1]:
+            out[index] = i
+            index +=1
+        self.consume_each(int(maxCorrIndex + self.syncAndPacketSize))
+        return (self.packetSize)
+       
+
